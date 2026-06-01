0276a644f03fa84e041cc05f51b6890ca41a3f8e
pietkaantoni <antos2007@poczta.fm>
2026-06-01 23:15:21 +0200

    Poprawa drobnych błędów

--- app/src/main/java/com/example/loan_for_lawn_mobile/data/api/ApiClient.java
+++ app/src/main/java/com/example/loan_for_lawn_mobile/data/api/ApiClient.java
@@ -1,5 +1,8 @@
 package com.example.loan_for_lawn_mobile.data.api;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,8 +13,15 @@ public class ApiClient {
 
     public static ApiService getClient() {
         if (nbpService == null) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .connectTimeout(15, TimeUnit.SECONDS)
+                    .readTimeout(15, TimeUnit.SECONDS)
+                    .writeTimeout(15, TimeUnit.SECONDS)
+                    .build();
+
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl(NBP_BASE_URL)
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
             nbpService = retrofit.create(ApiService.class);
